59655b98530d4657986a5d74411fab57514a73cd
tanghongwei <hongwei.tang@rydeen.com.cn>
2025-11-28 13:20:49 +0800

    feat(home): 添加标语并优化样式导入路径
    - 在首页HTML中新增标语 "Authentic Chinese Flavors, Crafted with Heart"
    - 更新SCSS文件中的px-to-rem导入路径为绝对路径
    - 移除HomeService中未使用的AppUrlService依赖
    - 注释掉HomeService中的轮播图请求逻辑
    - 调整全局样式中的媒体查询配置以适配不同屏幕尺寸

--- src/app/features/home/service/home.service.ts
+++ src/app/features/home/service/home.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, signal } from '@angular/core';
-import { AppUrlService } from '@app/shared/services/app.url.service';
 import { AbstractAppService } from '@app/shared/abstracts/abstract.app.service';
 import { CarouselImageResponseVO,StoreCarouselConstants} from '@app/shared/types/common.types';
 import { AppUrl } from '@app/core/constants/app.url';
@@ -11,7 +10,7 @@ import { DateUtils } from '@app/shared/util/date-utils.service';
 @Injectable({ providedIn: 'root' })
 export class HomeService extends AbstractAppService {
    diningPeople = signal<number>(0);
-    constructor(private appUrlService: AppUrlService,private dateUtils: DateUtils,) {
+    constructor(private dateUtils: DateUtils,) {
         super()
     }
 
@@ -20,12 +19,12 @@ export class HomeService extends AbstractAppService {
     }
 
     async getCarouselImages() {
-        const res = await this.request<CarouselImageResponseVO[]>(AppUrl.GET_RESOURCE, {
-            pageCode: StoreCarouselConstants.PAGE_CODE,
-            operationAreaCode: StoreCarouselConstants.OPERATION_AREA_CODE,
-            nowDate: this.dateUtils.formatDateTime(new Date()),
-        });
-        const data = res.data[0] || {};
+        // const res = await this.request<CarouselImageResponseVO[]>(AppUrl.GET_RESOURCE, {
+        //     pageCode: StoreCarouselConstants.PAGE_CODE,
+        //     operationAreaCode: StoreCarouselConstants.OPERATION_AREA_CODE,
+        //     nowDate: this.dateUtils.formatDateTime(new Date()),
+        // });
+        // const data = res.data[0] || {};
     }
 
     setDiningPeople(num: number) {
